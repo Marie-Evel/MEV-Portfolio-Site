7f3e1f57cea4515445bf45aadec83ed453a15c35
Marie-Eve Vesel <me.vesel@gmail.com>
2020-05-06 22:13:05 -0500

    Move code specific to index page to a separate js file

---
+++ js/index.js
@@ -0,0 +1,47 @@
+
+var logo = document.getElementById('mev-logo-animation');
+var play = document.getElementById('play-animation-button');
+var overlay = logo.getElementsByClassName('overlay')[0];
+
+function animateLogo() {
+  play.classList.add('hide');
+  logo.classList.add('animate');
+  overlay.classList.add('hide-overlay');
+
+  setTimeout( function() {
+    play.classList.remove('hide');
+    logo.classList.remove('animate');
+    overlay.classList.remove('hide-overlay');
+  }, 6000);
+}
+
+function isVisible(targetElement, elementHeight, offsetTop, offsetBottom) {
+  var gridTop = offsetTop;
+  var gridBottom = window.innerHeight - offsetBottom;
+  var elementTop = targetElement.getBoundingClientRect().top;
+  var elementBottom = targetElement.getBoundingClientRect().bottom;
+
+  if ( elementTop < gridTop || elementBottom < gridBottom ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function neverPlayed() {
+  if ( logo.classList.contains('played-on-scroll') ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+var mevStudy = document.getElementById('branding-case-study');
+
+// window.addEventListener('scroll', function() {
+window.onscroll = function() {
+  if ( isVisible(mevStudy, 250, 85, 80) && neverPlayed() ) {
+    animateLogo()
+    logo.classList.add('played-on-scroll');
+  };
+}
